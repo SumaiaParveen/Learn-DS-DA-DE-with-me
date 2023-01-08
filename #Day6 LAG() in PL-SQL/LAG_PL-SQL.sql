9510181e0a6954363caa00401693f38e977960fa
Sumaia Parveen <56892114+SumaiaParveen@users.noreply.github.com>
2023-01-07 22:28:38 -0800

    Create LAG_PL-SQL.sql

---
+++ #Day6 LAG() in PL-SQL/LAG_PL-SQL.sql
@@ -0,0 +1,20 @@
+--- create a table called sales with three columns: store_id, sale_date, and total_sales:
+
+CREATE TABLE sales (
+  store_id INTEGER,
+  sale_date DATE,
+  total_sales INTEGER
+);
+
+INSERT INTO sales (store_id, sale_date, total_sales)
+VALUES (1, '2022-01-01', 100),
+       (1, '2022-01-02', 120),
+       (1, '2022-01-03', 130),
+       (2, '2022-01-01', 90),
+       (2, '2022-01-03', 110);
+
+--- use the LAG() function to shift the values in the total_sales column by one row, and it adds a new column called prev_sales to the table containing the shifted values. The LAG() function is applied within each store_id group (defined by the PARTITION BY clause) and is ordered by the sale_date column (defined by the ORDER BY clause). The default value of 0 is used for the first row within each group.
+
+SELECT store_id, sale_date, total_sales,
+       LAG(total_sales, 1, 0) OVER (PARTITION BY store_id ORDER BY sale_date) AS prev_sales
+FROM sales;
